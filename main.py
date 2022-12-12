584a5654d7dd03f9507cf5c2604d288e5a9712a8
Koala610 <vladli2000610@gmail.com>
2022-12-12 19:20:17 +0600

    fix: concurrent launch

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from config import asyncio, uvicorn
 from src import dp, app
 
 @app.on_event("startup")
-def start_bot():
-    dp.start_polling()
+async def start_bot():
+    asyncio.create_task(dp.start_polling())
 
 if __name__ == "__main__":
     asyncio.create_task(uvicorn.run(app=app, host="localhost", port=8080))
