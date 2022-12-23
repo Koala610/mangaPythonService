221752b07ac203e3e46e49e634bc7efd26294766
Koala610 <vladli2000610@gmail.com>
2022-12-23 13:25:58 +0600

    add: delay before sending request to getting bookmarks. Bookmarks now store in dispatcher storage | delete: unused files

--- src/service/_bot/controllers/manga_controller.py
+++ src/service/_bot/controllers/manga_controller.py
@@ -1,4 +1,5 @@
 import src.service._bot.models.bookmarks as bookmark_actions
+import datetime
 
 from aiogram import types
 
@@ -14,12 +15,18 @@ from ..views.bookmarks import create_bookmark_response
 )
 async def get_bookmarks(message: types.Message):
     user_id = message.from_user.id
-    bookmarks = await bookmark_actions.get_bookmarks(user_id, limit=50, offset=0)
+    storage_data = await dp.storage.get_data(user=user_id)
+    bookmarks, is_renewed = await bookmark_actions.get_bookmarks(user_id, storage_data=storage_data)
+    if is_renewed:
+        storage_data = {
+            "timestamp": datetime.datetime.now(),
+            "bookmarks": bookmarks
+        }
+        logger.info("Made request")
+        await dp.storage.set_data(user=user_id, data=storage_data)
     if bookmarks is None:
         await telegram_bot.send_message(user_id, "Ошибка при получении закладок. Попробуйте снова ввести свои данные через /account")
         return
     for bookmark in bookmarks:
         text, markup = create_bookmark_response(bookmark)
         await telegram_bot.send_message(user_id, text, reply_markup=markup)
-
-
--- src/service/_bot/models/bookmarks.py
+++ src/service/_bot/models/bookmarks.py
@@ -1,5 +1,7 @@
 import aiohttp
+import datetime
 
+from src.config import REQUEST_WAITING_TIME
 from src.repository import user_repository
 from src.service.rm_service import rm_service
 from src.service.http_client.exceptions import NotAuthorized
@@ -7,10 +9,17 @@ from src import logger
 from typing import Optional
 
 
-async def get_bookmarks(user_id: int, limit: int = 50, offset: int = 0) -> Optional[list]:
-    try:
-        bookmarks = await rm_service.get_bookmarks(user_id, limit, offset)
-    except NotAuthorized:
-        return None
-    user_repository.update(user_id, bookmarks_hash=hash(tuple(bookmarks)))
-    return bookmarks
+async def get_bookmarks(user_id: int, storage_data, limit: int = 50, offset: int = 0) -> Optional[list]:
+
+    timestamp = storage_data.get("timestamp")
+    bookmarks = storage_data.get("bookmarks")
+    if timestamp is None or bookmarks is None or datetime.datetime.now() - storage_data.get("timestamp") > datetime.timedelta(minutes=REQUEST_WAITING_TIME):
+        try:
+            bookmarks = await rm_service.get_bookmarks(user_id, limit, offset)
+        except NotAuthorized:
+            return None, None
+        if bookmarks is None:
+            return None, None
+        user_repository.update(user_id, bookmarks_hash=hash(tuple(bookmarks)))
+        return bookmarks, True
+    return bookmarks, False
