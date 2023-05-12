c5cf660cf416effd834d5fb409773a8b8502b3a0
vladlen-li-idf <vladlen.li@solva.kz>
2023-05-12 18:20:14 +0600

    feat: new endpoints

--- src/service/_bot_http_cover/services/user_service.py
+++ src/service/_bot_http_cover/services/user_service.py
@@ -3,3 +3,7 @@ from src.repository.mysql import user_repository
 def get_users():
     users = user_repository.find_all()
     return users
+
+def get_user_by_id(user_id: str):
+    user = user_repository.find_by_id(user_id)
+    return user
